98ea5b97eb1757a44e9eca91d28b12b2ad1bc6a2
Greg Blackburn <greg@zazatechnologies.com>
2025-09-01 22:03:59 +0200

    chore: release teacher-first homepage and fixes

--- src/components/site/snippet-tool.tsx
+++ src/components/site/snippet-tool.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useTranslations } from 'next-intl';
+// import { useTranslations } from 'next-intl';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Textarea } from '@/components/ui/textarea';
