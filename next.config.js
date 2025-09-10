db5eb72fe6b3ca8394a7757fd2a8dc81a737c08d
Greg Blackburn <greg@zazatechnologies.com>
2025-09-10 20:10:16 +0200

    Add Next.js redirects() for / -> /en

---
+++ next.config.js
@@ -0,0 +1,10 @@
+﻿/** @type {import("next").NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: "/", destination: "/en", permanent: true }
+    ];
+  },
+};
+
+module.exports = nextConfig;
