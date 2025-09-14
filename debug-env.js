84581b5941adfac2fd8c1e2ef6327a0f5a9a5712
Greg Blackburn <greg@zazatechnologies.com>
2025-09-14 17:50:46 +0200

    freeze: i18n baseline (messages + request config)

---
+++ debug-env.js
@@ -0,0 +1,6 @@
+// Simple debug script to check environment variables
+console.log('=== Environment Variables Debug ===');
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('NEXT_PUBLIC_ENABLE_ZARA:', process.env.NEXT_PUBLIC_ENABLE_ZARA);
+console.log('NEXT_PUBLIC_SITE_URL:', process.env.NEXT_PUBLIC_SITE_URL);
+console.log('====================================');
